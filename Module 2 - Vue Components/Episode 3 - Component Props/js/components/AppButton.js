5ef486a1a33509de439971272eb232ba2a61bcbf
Tiago Santos <tsantos@lynixt.pt>
2025-11-11 22:55:40 +0000

    Vue Components - Episode 3

---
+++ Module 2 - Vue Components/Episode 3 - Component Props/js/components/AppButton.js
@@ -0,0 +1,26 @@
+export default {
+	template: `
+		<button
+			:class="{
+				'border rounded px-5 py-2 disabled:cursor-not-allowed': true,
+				'bg-blue-200 hover:bg-blue-400': type === 'primary',
+				'bg-purple-200 hover:bg-purple-400': type === 'secondary',
+				'bg-gray-200 hover:bg-gray-400': type === 'muted',
+				'is_loading': processing,
+			}"
+			:disabled="processing"
+		>
+			<slot />	
+		</button>
+	`,
+	props: {
+		type: {
+			type: String,
+			default: 'primary'
+		},
+		processing: {
+			type: Boolean,
+			default: false
+		}
+	}
+}
