304e53ded8861491b77bd7ebbc421eb1cda75b53
Tiago Santos <tsantos@lynixt.pt>
2025-11-11 23:26:55 +0000

    Vue Components - Episode 4

---
+++ Module 2 - Vue Components/Episode 4 - Bring it All Together/js/components/Assignments.js
@@ -0,0 +1,30 @@
+import AssignmentList from "./AssignmentList.js";
+
+export default {
+	components: {
+		AssignmentList
+	},
+	template: `
+		<section class="space-y-6">
+			<assignment-list :assignments="filters.inProgress" title="In Progress"></assignment-list>
+			<assignment-list :assignments="filters.completed" title="Completed"></assignment-list>
+		</section>
+	`,
+	data() {
+		return {
+			assignments: [
+				{ name: 'Finish project', complete: false, id: 1 },
+				{ name: 'Read chapter 4', complete: false, id: 2 },
+				{ name: 'Turn in homework', complete: false, id: 3 },
+			]
+		}
+	},
+	computed: {
+		filters() {
+			return {
+				inProgress: this.assignments.filter(a => !a.complete),
+				completed: this.assignments.filter(a => a.complete),
+			}
+		}
+	},
+}
