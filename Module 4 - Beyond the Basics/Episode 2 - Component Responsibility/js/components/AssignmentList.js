8506276074ba8c5985565ebf4b424eb7d2a0776c
Tiago Santos <tsantos@lynixt.pt>
2025-11-12 21:13:16 +0000

    Beyond the Basics - Episode 2

---
+++ Module 4 - Beyond the Basics/Episode 2 - Component Responsibility/js/components/AssignmentList.js
@@ -0,0 +1,47 @@
+import Assignment from "./Assignment.js";
+import AssignmentTags from "./AssignmentTags.js";
+
+export default {
+	components: {
+		Assignment,
+		AssignmentTags
+	},
+	template: `
+		<section v-show="assignments.length">
+			<h2 class="font-bold mb-2">
+				{{ title }}
+				<span>({{ filterAssignments.length }})</span>
+			</h2>
+			<assignment-tags
+				:initial-tags="assignments.map(a => a.tag)"
+				:current-tag="currentTag"
+				@change="currentTag = $event"
+			/>
+			<ul class="border border-gray-600 divide-y divide-gray-600 mt-6">
+				<assignment
+					v-for="assignment in filterAssignments"
+					:key="assignment.id"
+					:assignment="assignment"
+				><assignment>
+			</ul>
+		</section>
+	`,
+	props: {
+		assignments: Array,
+		title: String
+	},
+	computed: {
+		filterAssignments() {
+			if (this.currentTag === 'all') {
+				return this.assignments
+			}
+
+			return this.assignments.filter(a => a.tag === this.currentTag)
+		}
+	},
+	data() {
+		return {
+			currentTag: 'all'
+		}
+	}
+}
