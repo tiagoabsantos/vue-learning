184f741afc7ba8d0cb60a02c606227ea2077ce2d
Tiago Santos <tsantos@lynixt.pt>
2025-12-03 21:47:49 +0000

    The Composition API - Two Mental Leaps to Script Setup

--- my-first-vue-project/src/views/HomeView.vue
+++ my-first-vue-project/src/views/HomeView.vue
@@ -1,9 +1,19 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import TheWelcome from '@/components/TheWelcome.vue'
+import { ref } from 'vue';
+
+let message = ref('Hello, World!');
+
+let doSomething = () => {
+  alert('Hi there!');
+}
 </script>
 
 <template>
   <main>
     <TheWelcome />
+
+    <p>{{ message }}</p>
+    <button @click="doSomething">Click me</button>
   </main>
 </template>
