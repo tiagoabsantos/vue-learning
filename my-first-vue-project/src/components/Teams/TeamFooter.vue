f60ab303e15a5f9c5c4a095a2998f4921879b321
Tiago Santos <tsantos@lynixt.pt>
2025-12-10 21:21:56 +0000

    Team Dashboard Exercise - Code Organization

---
+++ my-first-vue-project/src/components/Teams/TeamFooter.vue
@@ -0,0 +1,11 @@
+<script setup>
+defineProps({
+	team: Object
+})
+</script>
+
+<template>
+	<footer class="mt-12 bg-gray-100 py-4 text-center">
+		<h5 class="font-semibold text-lg">{{ team.name }} - {{ team.spots }} Member Team</h5>
+	</footer>
+</template>
---
+++ my-first-vue-project/src/views/TeamView.vue
@@ -0,0 +1,14 @@
+<script setup>
+import TeamFooter from '@/components/Teams/TeamFooter.vue';
+import TeamHeader from '@/components/Teams/TeamHeader.vue'
+import TeamMembers from '@/components/Teams/TeamMembers.vue'
+import team from '@/team.json'
+</script>
+
+<template>
+	<TeamHeader :team="team" />
+	<div class="place-self-center flex flex-col gap-y-3" style="width: 725px;">
+		<TeamMembers :team="team" />
+	</div>
+	<TeamFooter :team="team" />
+</template>
